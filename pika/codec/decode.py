896e71da98fc9acf3ac8bd73519b1d3991cd75be
Gavin M. Roy <gmr@myyearbook.com>
2011-03-30 14:38:31 -0400

    Implement decoding of all data types. Add test coverage for 100% of encode and decode. Fix table decode test.

--- pika/codec/decode.py
+++ pika/codec/decode.py
@@ -3,8 +3,205 @@
 # For copyright and licensing please refer to COPYING.
 #
 # ***** END LICENSE BLOCK *****
+"""
+AMQP Data Decoder
 
-__author__ = 'Gavin M. Roy'
-__email__ = 'gmr@myyearbook.com'
-__date__ = '2011-03-29'
+Functions for decoding data of various types including field tables and arrays
+"""
+__author__ = 'Gavin M. Roy <gmr@myyearbook.com>'
+__since__ = '2011-03-30'
 
+from decimal import Decimal
+from struct import unpack_from
+from time import gmtime
+
+
+def boolean(value):
+    """
+    Decode a boolean value
+
+    Parameters:
+    - str
+
+    Returns:
+    - tuple of bytes used and a bool value
+    """
+
+    return 1, bool(unpack_from('>B', value)[0])
+
+
+def decimal(value):
+    """
+    Decode a decimal value
+
+    Parameters:
+    - str
+
+    Returns:
+    - tuple of bytes used and a Decimal value
+    """
+    decimals = unpack_from('>B', value)[0]
+    raw = unpack_from('>i', value, 1)[0]
+    return 5, Decimal(raw) * (Decimal(10) ** -decimals)
+
+
+def floating_point(value):
+    """
+    Decode a floating point value
+
+    Parameters:
+    - str
+
+    Returns:
+    - tuple of bytes used and a float value
+    """
+    return 4, unpack_from('>f', value)[0]
+
+
+def long_int(value):
+    """
+    Decode a long integer value
+
+    Parameters:
+    - str
+
+    Returns:
+    - tuple of bytes used and an int value
+    """
+    return 4, unpack_from('>l', value)[0]
+
+
+def long_long_int(value):
+    """
+    Decode a long-long integer value
+
+    Parameters:
+    - str
+
+    Returns:
+    - tuple of bytes used and an int value
+    """
+    return 8, unpack_from('>q', value)[0]
+
+
+def short_int(value):
+    """
+    Decode a short integer value
+
+    Parameters:
+    - str
+
+    Returns:
+    - tuple of bytes used and an int value
+    """
+    return 2, unpack_from('>h', value)[0]
+
+
+def string(value):
+    """
+    Decode a string value
+
+    Parameters:
+    - str
+
+    Returns:
+    - tuple of bytes used and an str value
+    """
+    length = unpack_from('>I', value)[0]
+    return length + 4, value[4:length + 4]
+
+
+def timestamp(value):
+    """
+    Decode a timestamp value
+
+    Parameters:
+    - str
+
+    Returns:
+    - tuple of bytes used and an struct_time value
+    """
+    return 8, gmtime(unpack_from('>Q', value)[0])
+
+
+def field_array(value):
+    """
+    Decode a field array value
+
+    Parameters:
+    - str
+
+    Returns:
+    - tuple of bytes used and a list of values
+    """
+    length = unpack_from('>l', value)[0]
+    offset = 4
+    data = list()
+    field_array_end = offset + length
+    while offset < field_array_end:
+        consumed, result = _decode_value(value[offset:])
+        offset += consumed
+        data.append(result)
+    return field_array_end, data
+
+
+def field_table(value):
+    """
+    Decode a field array value
+
+    Parameters:
+    - str
+
+    Returns:
+    - tuple of bytes used and a list of values
+    """
+    length = unpack_from('>l', value)[0]
+    offset = 4
+    data = dict()
+    field_table_end = offset + length
+    while offset < field_table_end:
+        keysize = unpack_from('B', value, offset)[0] + 1
+        key = value[offset + 1:offset + keysize]
+        offset += keysize
+        consumed, result = _decode_value(value[offset:])
+        offset += consumed
+        data[key] = result
+    return field_table_end, data
+
+
+def _decode_value(value):
+    """
+    Takes in a value looking at the first byte to determine which decoder to
+    use.
+
+    Parameters:
+    - str
+
+    Returns:
+    - tuple of bytes consumed and mixed value based on field type
+    """
+    # Determine the field type and encode it
+    if value[0] == 'A':
+        consumed, result = field_array(value[1:])
+    elif value[0] == 'D':
+        consumed, result = decimal(value[1:])
+    elif value[0] == 'f':
+        consumed, result = floating_point(value[1:])
+    elif value[0] == 'F':
+        consumed, result = field_table(value[1:])
+    elif value[0] == 'I':
+        consumed, result = long_int(value[1:])
+    elif value[0] == 'L':
+        consumed, result = long_long_int(value[1:])
+    elif value[0] == 't':
+        consumed, result = boolean(value[1:])
+    elif value[0] == 'T':
+        consumed, result = timestamp(value[1:])
+    elif value[0] == 's' or value[0] == 'S':
+        consumed, result = string(value[1:])
+    elif value[0] == 'U':
+        consumed, result = short_int(value[1:])
+    else:
+        raise ValueError("Unknown type: %s", value[0])
+    # Return the bytes used and the decoded value
+    return consumed + 1, result
--- tests/codec_tests.py
+++ tests/codec_tests.py
@@ -13,7 +13,212 @@ import pika.codec as codec
 
 from datetime import datetime
 from decimal import Decimal
+from time import mktime, struct_time
 
+# -- Helpers --
+
+def compare_lists(source, dest, method='Unknown'):
+    for position in xrange(0, len(source)):
+
+        # Validate decimal, int, long, str, nothing special needed
+        if isinstance(source[position], Decimal) or \
+           isinstance(source[position], int) or \
+           isinstance(source[position], long) or \
+           isinstance(source[position], str):
+            if source[position] != dest[position]:
+                assert False, \
+                "%s did not properly decode item in position %i '%r' vs '%r'" \
+                % (method, position, source[position], dest[position])
+
+        elif isinstance(source[position], float):
+            if round(source[position], 2) != round(dest[position], 2):
+                assert False, \
+                "%s did not properly decode item in position %i '%r' vs '%r'" \
+                % (method, position, source[position], dest[position])
+
+        elif isinstance(source[position], struct_time):
+            if mktime(source[position]) != mktime(dest[position]):
+                assert False, \
+                "%s did not properly decode item in position %i '%r' vs '%r'" \
+                % (method, position, source[position], dest[position])
+
+        elif isinstance(source[position], list):
+            compare_lists(source[position], dest[position], method)
+
+        elif isinstance(source[position], dict):
+            compare_dicts(source[position], dest[position], method)
+
+        else:
+            assert False, "Unchecked item in position %i: %r" % \
+                          (position, source[position])
+
+
+def compare_dicts(source, dest, method='Unknown'):
+
+    for key in source.keys():
+
+        # Validate we have the key
+        if key not in dest:
+            assert False, "%s did not properly decode dict: %s missing: %r" % \
+                          (method, key, dest)
+
+        # Validate decimal, int, long, str, nothing special needed
+        if isinstance(source[key], Decimal) or \
+           isinstance(source[key], int) or \
+           isinstance(source[key], long) or \
+           isinstance(source[key], str):
+            if source[key] != dest[key]:
+                assert False, \
+                "%s did not properly decode item %s '%r' vs '%r'" \
+                % (method, key, source[key], dest[key])
+
+        elif isinstance(source[key], float):
+            if round(source[key], 2) != round(dest[key], 2):
+                assert False, \
+                "%s did not properly decode item %s '%r' vs '%r'" \
+                % (method, key, source[key], dest[key])
+
+        elif isinstance(source[key], struct_time):
+            if mktime(source[key]) != mktime(dest[key]):
+                assert False, \
+                "%s did not properly decode item %s '%r' vs '%r'" \
+                % (method, key, source[key], dest[key])
+
+        elif isinstance(source[key], list):
+            compare_lists(source[key], dest[key], method)
+
+        elif isinstance(source[key], dict):
+            compare_dicts(source[key], dest[key], method)
+
+        else:
+            assert False, "Unchecked item %s: %r" % \
+                          (key, source[key])
+
+# -- Decoding Tests --
+
+def test_decode_bool_false():
+    value = '\x00'
+    length, check = codec.decode.boolean(value)
+    if not isinstance(check, bool):
+        assert False, "decode.boolean did not return a bool: %r" % check
+    if check:
+        assert False, "decode.boolean did not return False: %r" % check
+
+
+def test_decode_bool_true():
+    value = '\x01'
+    length, check = codec.decode.boolean(value)
+    if not isinstance(check, bool):
+        assert False, "decode.boolean did not return a bool: %r" % check
+    if not check:
+        assert False, "decode.boolean did not return True: %r" % check
+
+
+def test_decode_decimal():
+    value = '\x05\x00\x04\xcb/'
+    length, check = codec.decode.decimal(value)
+    if not isinstance(check, Decimal):
+        assert False, "decode.decimal did not return a Decimal: %r" % check
+    if check != Decimal('3.14159'):
+        assert False, "decode.decimal did not return 3.14159: %r" % check
+
+
+def test_decode_float():
+    value = '@I\x0f\xd0'
+    length, check = codec.decode.floating_point(value)
+    if not isinstance(check, float):
+        assert False, "decode.floating_point did not return a float: %r" % \
+                      check
+    if round(check, 5) != 3.14159:
+        assert False, "decode.floating_point did not return 3.14159: %r" % \
+                      round(check, 5)
+
+
+def test_decode_long():
+    value = '\x7f\xff\xff\xff'
+    length, check = codec.decode.long_int(value)
+    if not isinstance(check, int) and not isinstance(check, long):
+        assert False, "decode.long_int did not return an int: %r" % check
+    if check != 2147483647:
+        assert False, "decode.long_int did not return 2147483647: %r" % \
+                      check
+
+
+def test_decode_long_long():
+    value = '\x7f\xff\xff\xff\xff\xff\xff\xf8'
+    length, check = codec.decode.long_long_int(value)
+    if not isinstance(check, int) and not isinstance(check, long):
+        assert False, "decode.long_int did not return an int: %r" % check
+    if check != 9223372036854775800:
+        assert False, \
+        "decode.long_long_int did not return 9223372036854775800: %r" % check
+
+
+def test_decode_short():
+    value = '\x7f\xff'
+    length, check = codec.decode.short_int(value)
+    if not isinstance(check, int):
+        assert False, "decode.short_int did not return an int: %r" % check
+    if check != 32767:
+        assert False, "decode.short_int did not return 32767: %r" % check
+
+
+def test_decode_string():
+    value = '\x00\x00\x00\n0123456789'
+    length, check = codec.decode.string(value)
+    if not isinstance(check, str):
+        assert False, "decode.string did not return a str: %r" % check
+    if check != '0123456789':
+        assert False, "decode.string did not return '0123456789': %r" % check
+
+
+def test_decode_timestamp():
+    value = '\x00\x00\x00\x00Ec)\x92'
+    length, check = codec.decode.timestamp(value)
+    if not isinstance(check, struct_time):
+        assert False, "decode.timestamp did not return a struct_time: %r" % \
+                      check
+    if mktime(check) != mktime((2006, 11, 21, 16, 30, 10, 1, 325, 0)):
+        assert False, \
+        "decode.timestamp did not return '2006, 11, 21, 16, 30, 10': %r" % \
+        check
+
+
+def test_decode_field_array():
+    value = '\x00\x00\x00<U\x00\x01I\x00\x00\xaf\xc8S\x00\x00\x00\x04TestT\
+\x00\x00\x00\x00Ec)\x92I\xbb\x9a\xca\x00D\x02\x00\x00\x01:f@H\xf5\xc3L\x00\x00\
+\x00\x00\xc4e5\xffL\x80\x00\x00\x00\x00\x00\x00\x08'
+    data = [1, 45000, 'Test',  datetime(2006, 11, 21, 16, 30, 10).timetuple(),
+            -1147483648, Decimal('3.14'), 3.14, long(3294967295),
+            -9223372036854775800]
+    length, check = codec.decode.field_array(value)
+    if not isinstance(check, list):
+        assert False, "decode.field_array did not return a list: %r" % check
+    compare_lists(data, check, 'decode.field_array')
+
+
+def test_decode_field_table():
+    value = '\x00\x00\x00\x92\x07longvalI6e&U\x08floatvlaf@H\xf5\xc3\x07\
+boolvalt\x01\x06strvalS\x00\x00\x00\x04Test\x06intvalU\x00\x01\x0ctimestampval\
+T\x00\x00\x00\x00Ec)\x92\x06decvalD\x02\x00\x00\x01:\x08arrayvalA\x00\x00\x00\
+\tU\x00\x01U\x00\x02U\x00\x03\x07dictvalF\x00\x00\x00\x0c\x03fooS\x00\x00\x00\
+\x03bar'
+    data = {'intval': 1,
+            'strval': 'Test',
+            'boolval': True,
+            'timestampval': datetime(2006, 11, 21, 16, 30, 10).timetuple(),
+            'decval': Decimal('3.14'),
+            'floatvla': 3.14,
+            'longval': long(912598613),
+            'dictval': {'foo': 'bar'},
+            'arrayval': [1, 2, 3]}
+    length, check = codec.decode.field_table(value)
+    if not isinstance(check, dict):
+        assert False, "decode.field_table did not return a list: %r" % check
+    compare_dicts(data, check, 'decode.field_table')
+
+
+# -- Encoding Tests --
 
 def test_encode_bool_wrong_type():
     try:
@@ -122,25 +327,25 @@ def test_encode_long_long_int_error():
 
 def test_encode_short_wrong_type():
     try:
-        codec.encode.short(3.141597)
+        codec.encode.short_int(3.141597)
     except ValueError:
         return
-    assert False, "encode.short did not raise a ValueError Exception"
+    assert False, "encode.short_int did not raise a ValueError Exception"
 
 
 def test_encode_short():
     check = 'U\x7f\xff'
-    value = codec.encode.short(32767)
+    value = codec.encode.short_int(32767)
     if value != check:
         assert False, "Encoded value does not match check value: %r" % value
 
 
 def test_encode_short_error():
     try:
-        codec.encode.short(32768)
+        codec.encode.short_int(32768)
     except ValueError:
         return
-    assert False, "encode.short did not raise a ValueError Exception"
+    assert False, "encode.short_int did not raise a ValueError Exception"
 
 
 def test_encode_string():
@@ -232,10 +437,10 @@ def test_encode_field_table_type_error():
 
 
 def test_encode_field_table():
-    check = '\x00\x00\x00\x91\x07longvalI6e&U\x08floatvlaf@H\xf5\xc3\x07\
+    check = 'F\x00\x00\x00\x92\x07longvalI6e&U\x08floatvlaf@H\xf5\xc3\x07\
 boolvalt\x01\x06strvalS\x00\x00\x00\x04Test\x06intvalU\x00\x01\x0ctimestampval\
 T\x00\x00\x00\x00Ec)\x92\x06decvalD\x02\x00\x00\x01:\x08arrayvalA\x00\x00\x00\
-\tU\x00\x01U\x00\x02U\x00\x03\x07dictval\x00\x00\x00\x0c\x03fooS\x00\x00\x00\
+\tU\x00\x01U\x00\x02U\x00\x03\x07dictvalF\x00\x00\x00\x0c\x03fooS\x00\x00\x00\
 \x03bar'
     data = {'intval': 1,
             'strval': 'Test',
@@ -249,3 +454,6 @@ T\x00\x00\x00\x00Ec)\x92\x06decvalD\x02\x00\x00\x01:\x08arrayvalA\x00\x00\x00\
     value = codec.encode.field_table(data)
     if value != check:
         assert False, "Encoded value does not match check value: %r" % value
+
+
+# -
