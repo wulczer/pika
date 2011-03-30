544efb11920a62fbc00db66d559bd6e52b89e774
Gavin M. Roy <gmr@myyearbook.com>
2011-03-29 22:32:52 -0400

    Initial setup.py and README.md for 2.0

---
+++ README.md
@@ -0,0 +1,11 @@
+Pika is a pure-python RabbitMQ client library that supports multiple
+methodologies for development including the utilization of common pythonic
+idioms in addition to callback-passing-style development commonly found
+in asynchronous development models. It is built on an asynchronous
+communication foundation which allows for both stand-alone development and
+easy integration with common asynchronous development frameworks such as
+Tornado and Twisted.
+
+Python versions supported: 2.6+
+
+Documentation is available at http://pika.github.com
--- setup.py
+++ setup.py
@@ -7,20 +7,21 @@
 from setuptools import setup
 
 long_description = """\
-Pika is a pure-Python implementation of the AMQP 0-9-1 protocol that
-tries to stay fairly independent of the underlying network support
-library. Pika was developed primarily for use with RabbitMQ, but
-should also work with other AMQP 0-9-1 brokers.
+Pika is a pure-python RabbitMQ client library that supports multiple
+methodologies for development including the utilization of common pythonic
+idioms in addition to callback-passing-style development commonly found
+in asynchronous development models. It is built on an asynchronous
+communication foundation which allows for both stand-alone development and
+easy integration with common asynchronous development frameworks such as
+Tornado and Twisted. Pika supports Python 2.6 through 3.2.
 """
 
 setup(name='pika',
       version='2.0.0',
-      description='Pika Python AMQP Client Library',
+      description='RabbitMQ client library',
       long_description=long_description,
-      author='Tony Garnock-Jones',
-      author_email='tonygarnockjones@gmail.com',
-      maintainer='Gavin M. Roy',
-      maintainer_email='gmr@myyearbook.com',
+      author='Tony Garnock-Jones, Gavin M. Roy',
+      author_email='tonygarnockjones@gmail.com, gmr@myyearbook.com',
       url='http://pika.github.com/',
       packages=['pika'],
       license='MPL v1.1 and GPL v2.0 or newer',
