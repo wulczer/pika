26c7c447024c652b2907244cf390b94b2229807b
Gavin M. Roy <gmr@myyearbook.com>
2011-03-30 00:09:08 -0400

    Add support for timestamps, field arrays, field tables and create 100% test coverage for encoding.
    Remove length in tuple returns

--- pika/codec/encode.py
+++ pika/codec/encode.py
@@ -6,8 +6,11 @@
 __author__ = 'Gavin M. Roy <gmr@myyearbook.com>'
 __since__ = '2011-03-29'
 
+from calendar import timegm
 from decimal import Decimal
+from datetime import datetime
 from struct import pack
+from time import struct_time
 
 
 def boolean(value):
@@ -18,11 +21,11 @@ def boolean(value):
     - bool value
 
     Returns:
-    - tuple of length and encoded value
+    - encoded value
     """
     if not isinstance(value, bool):
         raise ValueError("bool type required")
-    return 2, pack('>cB', 't', int(value))
+    return pack('>cB', 't', int(value))
 
 
 def decimal(value):
@@ -33,7 +36,7 @@ def decimal(value):
     - bool value
 
     Returns:
-    - tuple of length and encoded value
+    - encoded value
     """
     if not isinstance(value, Decimal):
         raise ValueError("decimal.Decimal type required")
@@ -41,9 +44,9 @@ def decimal(value):
     if value._exp < 0:
         decimals = -value._exp
         raw = int(value * (Decimal(10) ** decimals))
-        return 6,  pack('>cBi', 'D', decimals, raw)
+        return pack('>cBi', 'D', decimals, raw)
     # per spec, the "decimals" octet is unsigned (!)
-    return 6, pack('>cBi', 'D', 0, int(value))
+    return pack('>cBi', 'D', 0, int(value))
 
 
 def floating_point(value):
@@ -54,12 +57,11 @@ def floating_point(value):
     - float value
 
     Returns:
-    - tuple of length and encoded value
+    - encoded value
     """
     if not isinstance(value, float):
         raise ValueError("float type required")
-    output = pack('>cf', 'f',  value)
-    return len(output), output
+    return pack('>cf', 'f',  value)
 
 
 def long_int(value):
@@ -70,13 +72,13 @@ def long_int(value):
     - long value
 
     Returns:
-    - tuple of length and encoded value
+    - encoded value
     """
     if not isinstance(value, long) and not isinstance(value, int):
         raise ValueError("int or long type required")
     if value < -2147483648 or value > 2147483647:
         raise ValueError("Long integer range: -2147483648 to 2147483647")
-    return 5, pack('>cl', 'I', value)
+    return pack('>cl', 'I', value)
 
 
 def long_uint(value):
@@ -87,13 +89,13 @@ def long_uint(value):
     - long value
 
     Returns:
-    - tuple of length and encoded value
+    - encoded value
     """
     if not isinstance(value, long) and not isinstance(value, int):
         raise ValueError("int or long type required")
     if value < -4294967295 or value > 4294967295:
         raise ValueError("Long integer range: -4294967295 to 4294967295")
-    return 5, pack('>cL', 'i', value)
+    return pack('>cL', 'i', value)
 
 
 def long_long_int(value):
@@ -104,14 +106,14 @@ def long_long_int(value):
     - long value
 
     Returns:
-    - tuple of length and encoded value
+    - encoded value
     """
     if not isinstance(value, long) and not isinstance(value, int):
         raise ValueError("int or long type required")
     if value < -9223372036854775808 or value > 9223372036854775807:
         raise ValueError("Unsigned long-long range: \
 -9223372036854775808 to 9223372036854775807")
-    return 9, pack('>cq', 'L', value)
+    return pack('>cq', 'L', value)
 
 
 def long_long_uint(value):
@@ -122,14 +124,14 @@ def long_long_uint(value):
     - long value
 
     Returns:
-    - tuple of length and encoded value
+    - encoded value
     """
     if not isinstance(value, long) and not isinstance(value, int):
         raise ValueError("int or long type required")
     if value < -18446744073709551615 or value > 18446744073709551615:
         raise ValueError("Unsigned long-long range: \
 -18446744073709551615 to 18446744073709551615")
-    return 9, pack('>cQ', 'l', value)
+    return pack('>cQ', 'l', value)
 
 
 def short(value):
@@ -140,13 +142,13 @@ def short(value):
     - int value
 
     Returns:
-    - tuple of length and encoded value
+    - encoded value
     """
     if not isinstance(value, int):
         raise ValueError("int type required")
     if value < -32768 or value > 32767:
         raise ValueError("Short range: -32768 to 32767")
-    return 3, pack('>ch', 'U', value)
+    return pack('>ch', 'U', value)
 
 
 def string(value):
@@ -157,11 +159,11 @@ def string(value):
     - str value
 
     Returns:
-    - tuple of length and encoded value
+    - encoded value
     """
     if not isinstance(value, str) and not isinstance(value, unicode):
         raise ValueError("str or unicode type required")
-    return 5 + len(value), pack('>cI', 'S', len(value)) + value
+    return pack('>cI', 'S', len(value)) + value
 
 
 def ushort(value):
@@ -172,10 +174,125 @@ def ushort(value):
     - int value
 
     Returns:
-    - tuple of length and encoded value
+    - encoded value
     """
     if not isinstance(value, int):
         raise ValueError("int type required")
     if value < -65535 or value > 65535:
         raise ValueError("Unsigned short range: -65535 to 65535")
-    return 3, pack('>cH', 'u', value)
+    return pack('>cH', 'u', value)
+
+
+def timestamp(value):
+    """
+    Encode an datetime object or struct_time
+
+    Parameters:
+    - datetime or struct_time value
+
+    Returns:
+    - encoded value
+    """
+    if isinstance(value, datetime):
+        value = value.timetuple()
+    if isinstance(value, struct_time):
+        return pack('>cQ', 'T', timegm(value))
+    raise ValueError("datetime or struct_time type required")
+
+
+def field_array(value):
+    """
+    Encode a field array from a dictionary
+
+    Parameters:
+    - list
+
+    Returns:
+    - encoded value
+    """
+    if not isinstance(value, list):
+        raise ValueError("list type required")
+
+    data = list()
+    for item in value:
+        data.append(_encode_value(item))
+
+    output = ''.join(data)
+    return pack('>cI', 'A', len(output)) + output
+
+
+def field_table(value):
+    """
+    Encode a field table from a dictionary
+
+    Parameters:
+    - dict
+
+    Returns:
+    - encoded value
+    """
+    # If there is no value, return a standard 4 null bytes
+    if not value:
+        return '\x00\x00\x00\x00'
+
+    if not isinstance(value, dict):
+        raise ValueError("dict type required, got %s", type(value))
+
+    # Iterate through all of the keys and encode the data into a table
+    data = list()
+    for key in value.keys():
+
+        # Append the field header / delimiter
+        data.append(pack('B', len(key)))
+        data.append(key)
+        try:
+            data.append(_encode_value(value[key]))
+        except ValueError as err:
+            raise ValueError("%s error: %s", key, err)
+
+    # Join all of the data together as a string
+    output = ''.join(data)
+    return pack('>I', len(output)) + output
+
+
+def _encode_value(value):
+    """
+    Takes a value of any type and tries to encode it with the proper encoder
+
+    Parameters:
+    - mixed
+
+    Returns:
+    - str
+    """
+    # Determine the field type and encode it
+    if isinstance(value, bool):
+        return boolean(value)
+    elif isinstance(value, int) or isinstance(value, long):
+        # Send the appropriately sized data value
+        if value > -32768 and value < 32767:
+            return short(int(value))
+        elif value > -65535 and value < 65535:
+            return ushort(int(value))
+        elif value > -2147483648 and value < 2147483647:
+            return long_int(long(value))
+        elif value > -4294967295 and value < 4294967295:
+            return long_uint(long(value))
+        elif value > -9223372036854775808 and value < 9223372036854775807:
+            return long_long_int(long(value))
+        elif value > -18446744073709551615 and value < 18446744073709551615:
+            return long_long_uint(long(value))
+    elif isinstance(value, Decimal):
+        return decimal(value)
+    elif isinstance(value, float):
+        return floating_point(value)
+    elif isinstance(value, str):
+        return string(value)
+    elif isinstance(value, datetime) or isinstance(value, struct_time):
+        return timestamp(value)
+    elif isinstance(value, dict):
+        return field_table(value)
+    elif isinstance(value, list):
+        return field_array(value)
+
+    raise ValueError("Unknown type: %s (%r)", type(value), value)
--- tests/codec_tests.py
+++ tests/codec_tests.py
@@ -11,6 +11,7 @@ __date__ = '2011-03-29'
 import test_support
 import pika.codec as codec
 
+from datetime import datetime
 from decimal import Decimal
 
 
@@ -24,18 +25,14 @@ def test_encode_bool_wrong_type():
 
 def test_encode_bool_false():
     check = 't\x00'
-    length, value = codec.encode.boolean(False)
-    if len(value) != length:
-        assert False, "Encoded value length doesn't match return length"
+    value = codec.encode.boolean(False)
     if value != check:
         assert False, "Encoded value does not match check value"
 
 
 def test_encode_bool_true():
     check = 't\x01'
-    length, value = codec.encode.boolean(True)
-    if len(value) != length:
-        assert False, "Encoded value length doesn't match return length"
+    value = codec.encode.boolean(True)
     if value != check:
         assert False, "Encoded value does not match check value"
 
@@ -50,9 +47,7 @@ def test_encode_decimal_wrong_type():
 
 def test_encode_decimal():
     check = 'D\x05\x00\x04\xcb/'
-    length, value = codec.encode.decimal(Decimal('3.14159'))
-    if len(value) != length:
-        assert False, "Encoded value length doesn't match return length"
+    value = codec.encode.decimal(Decimal('3.14159'))
     if value != check:
         assert False, "Encoded value does not match check value"
 
@@ -67,9 +62,7 @@ def test_encode_floating_point_type():
 
 def test_encode_float():
     check = 'f@I\x0f\xd0'
-    length, value = codec.encode.floating_point(float(3.14159))
-    if len(value) != length:
-        assert False, "Encoded value length doesn't match return length"
+    value = codec.encode.floating_point(float(3.14159))
     if value != check:
         assert False, "Encoded value does not match check value"
 
@@ -84,9 +77,7 @@ def test_encode_long_int_wrong_type():
 
 def test_encode_long_int():
     check = 'I\x7f\xff\xff\xff'
-    length, value = codec.encode.long_int(long(2147483647))
-    if len(value) != length:
-        assert False, "Encoded value length doesn't match return length"
+    value = codec.encode.long_int(long(2147483647))
     if value != check:
         assert False, "Encoded value does not match check value"
 
@@ -109,9 +100,7 @@ def test_encode_long_uint_wrong_type():
 
 def test_encode_long_uint():
     check = 'i\xff\xff\xff\xff'
-    length, value = codec.encode.long_uint(long(4294967295))
-    if len(value) != length:
-        assert False, "Encoded value length doesn't match return length"
+    value = codec.encode.long_uint(long(4294967295))
     if value != check:
         assert False, "Encoded value does not match check value"
 
@@ -134,9 +123,7 @@ def test_encode_long_long_int_wrong_type():
 
 def test_encode_long_long_int():
     check = 'L\x7f\xff\xff\xff\xff\xff\xff\xf8'
-    length, value = codec.encode.long_long_int(long(9223372036854775800))
-    if len(value) != length:
-        assert False, "Encoded value length doesn't match return length"
+    value = codec.encode.long_long_int(long(9223372036854775800))
     if value != check:
         assert False, "Encoded value does not match check value"
 
@@ -159,9 +146,7 @@ def test_encode_long_long_uint_wrong_type():
 
 def test_encode_long_long_uint():
     check = 'l\xff\xff\xff\xff\xff\xff\xff\xff'
-    length, value = codec.encode.long_long_uint(long(18446744073709551615))
-    if len(value) != length:
-        assert False, "Encoded value length doesn't match return length"
+    value = codec.encode.long_long_uint(long(18446744073709551615))
     if value != check:
         assert False, "Encoded value does not match check value"
 
@@ -184,9 +169,7 @@ def test_encode_short_wrong_type():
 
 def test_encode_short():
     check = 'U\x7f\xff'
-    length, value = codec.encode.short(32767)
-    if len(value) != length:
-        assert False, "Encoded value length doesn't match return length"
+    value = codec.encode.short(32767)
     if value != check:
         assert False, "Encoded value does not match check value"
 
@@ -209,9 +192,7 @@ def test_encode_ushort_wrong_type():
 
 def test_encode_ushort():
     check = 'u\xff\xff'
-    length, value = codec.encode.ushort(65535)
-    if len(value) != length:
-        assert False, "Encoded value length doesn't match return length"
+    value = codec.encode.ushort(65535)
     if value != check:
         assert False, "Encoded value does not match check value"
 
@@ -226,8 +207,112 @@ def test_encode_ushort_error():
 
 def test_encode_string():
     check = 'S\x00\x00\x00\n0123456789'
-    length, value = codec.encode.string("0123456789")
-    if len(value) != length:
-        assert False, "Encoded value length doesn't match return length"
+    value = codec.encode.string("0123456789")
     if value != check:
         assert False, "Encoded value does not match check value"
+
+
+def test_encode_unicode():
+    check = 'S\x00\x00\x00\n0123456789'
+    value = codec.encode.string(unicode("0123456789"))
+    if value != check:
+        assert False, "Encoded value does not match check value"
+
+
+def test_encode_string_error():
+    try:
+        codec.encode.string(12345.12434)
+    except ValueError:
+        return
+    assert False, "encode.string did not raise a ValueError Exception"
+
+
+def test_encode_timestamp_from_datetime():
+    check = 'T\x00\x00\x00\x00Ec)\x92'
+    value = datetime(2006, 11, 21, 16, 30, 10)
+    value = codec.encode.timestamp(value)
+    if value != check:
+        assert False, "Encoded value does not match check value: %r" % value
+
+
+def test_encode_timestamp_from_struct_time():
+    check = 'T\x00\x00\x00\x00Ec)\x92'
+    value = datetime(2006, 11, 21, 16, 30, 10).timetuple()
+    value = codec.encode.timestamp(value)
+    if value != check:
+        assert False, "Encoded value does not match check value: %r" % value
+
+
+def test_encode_timestamp_error():
+    try:
+        codec.encode.timestamp("hi")
+    except ValueError:
+        return
+    assert False, "encode.timestamp did not raise a ValueError Exception"
+
+
+def test_encode_field_array_error():
+    try:
+        codec.encode.field_array("hi")
+    except ValueError:
+        return
+    assert False, "encode.field_array did not raise a ValueError Exception"
+
+
+def test_encode_field_array():
+    check = 'A\x00\x00\x00?U\x00\x01u\xaf\xc8S\x00\x00\x00\x04TestT\x00\x00\
+\x00\x00Ec)\x92I\xbb\x9a\xca\x00D\x02\x00\x00\x01:f@H\xf5\xc3i\xc4e5\xffL\x80\
+\x00\x00\x00\x00\x00\x00\x08l\xff\xff\xff\xff\xff\xff\xff\xf5'
+    data = [1, 45000, 'Test', datetime(2006, 11, 21, 16, 30, 10),
+            -1147483648, Decimal('3.14'), 3.14, long(3294967295),
+            -9223372036854775800, 18446744073709551605]
+    value = codec.encode.field_array(data)
+    if value != check:
+        assert False, "Encoded value does not match check value: %r" % value
+
+
+def test_encode_field_table_error():
+    try:
+        codec.encode.field_table({'test': object()})
+    except ValueError:
+        return
+    assert False, "encode.field_table did not raise a ValueError Exception"
+
+
+def test_encode_field_table():
+    check = '\x00\x00\x00X\x07longvalI6e&U\x08floatvlaf@H\xf5\xc3\x06strvalS\
+\x00\x00\x00\x04Test\x06intvalU\x00\x01\x0ctimestampvalT\x00\x00\x00\x00Ec)\
+\x92\x06decvalD\x02\x00\x00\x01:'
+    data = {'intval': 1,
+            'strval': 'Test',
+            'timestampval': datetime(2006, 11, 21, 16, 30, 10),
+            'decval': Decimal('3.14'),
+            'floatvla': 3.14,
+            'longval': long(912598613)}
+    value = codec.encode.field_table(data)
+    if value != check:
+        assert False, "Encoded value does not match check value: %r" % value
+
+def test_doctest():
+    r'''
+    >>> codec.encode.field_table(None)
+    '\x00\x00\x00\x00'
+    >>> codec.encode.field_table({})
+    '\x00\x00\x00\x00'
+    >>> codec.encode.field_table({'a':1, 'c':1, 'd':'x', 'e':{}})
+    '\x00\x00\x00\x18\x01aU\x00\x01\x01cU\x00\x01\x01e\x00\x00\x00\x00\x01dS\x00\x00\x00\x01x'
+    >>> codec.encode.field_table({'a':Decimal('1.0')})
+    '\x00\x00\x00\x08\x01aD\x00\x00\x00\x00\x01'
+    >>> codec.encode.field_table({'a':Decimal('5E-3')})
+    '\x00\x00\x00\x08\x01aD\x03\x00\x00\x00\x05'
+    >>> codec.encode.field_table({'a':datetime(2010,12,31,23,58,59)})
+    '\x00\x00\x00\x0b\x01aT\x00\x00\x00\x00M\x1enC'
+    >>> codec.encode.field_table({'test':Decimal('-0.01')})
+    '\x00\x00\x00\x0b\x04testD\x02\xff\xff\xff\xff'
+    >>> codec.encode.field_table({'a':-1, 'b':[1,2,3,4,-1],'g':-1})
+    '\x00\x00\x00 \x01aU\xff\xff\x01bA\x00\x00\x00\x0fU\x00\x01U\x00\x02U\x00\x03U\x00\x04U\xff\xff\x01gU\xff\xff'
+    >>> codec.encode.field_table({'a': 4611686018427387904L, 'b': -4611686018427387904L})
+    '\x00\x00\x00\x16\x01aL@\x00\x00\x00\x00\x00\x00\x00\x01bL\xc0\x00\x00\x00\x00\x00\x00\x00'
+    >>> codec.encode.field_table({'a': True, 'b': False})
+    '\x00\x00\x00\x08\x01at\x01\x01bt\x00'
+    '''
